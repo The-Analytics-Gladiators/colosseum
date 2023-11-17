330adfd7074beb85391833b18d935fa4d7e6ef0f
stoffel <nosuchmail.com>
2023-11-17 19:32:33 +0100

    tracer with extended data types

--- src/test/kotlin/io/gladiators/oneinch/TracerKtTest.kt
+++ src/test/kotlin/io/gladiators/oneinch/TracerKtTest.kt
@@ -1,22 +1,45 @@
 package io.gladiators.oneinch
 
+import io.github.resilience4j.kotlin.ratelimiter.RateLimiterRegistry
+import io.github.resilience4j.kotlin.ratelimiter.executeFunction
+import io.github.resilience4j.kotlin.ratelimiter.withRateLimiterConfig
 import io.gladiators.web3.Chain
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.engine.test.logging.trace
+import io.kotest.datatest.withData
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.property.forAll
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 class TracerKtTest : FunSpec({
 
-    test("tracer smoketest") {
-        withOneInchTracer(Chain.BSC) {
-            val trace = txTrace(33522898.toBigInteger(), "0x754144a308a69447a4cd90f9527773da1d9bf3ad7a78c441dbff8d4691b8e2c7")
-            trace.from shouldNotBe ""
-            trace.to shouldNotBe ""
-            trace.calls shouldHaveSize 5
-        }
+    threads = 2
 
+    val rateLimiterRegistry = RateLimiterRegistry {
+        withRateLimiterConfig {
+            limitForPeriod(1)
+            limitRefreshPeriod(1.4.seconds.toJavaDuration())
+            timeoutDuration(5.seconds.toJavaDuration())
+        }
+    }
+
+    val rateLimiter = rateLimiterRegistry.rateLimiter("oneinch")
+
+    context("tracer smoke") {
+        withOneInch(Chain.BSC) {
+            withData(
+                33522898.toBigInteger() to "0x754144a308a69447a4cd90f9527773da1d9bf3ad7a78c441dbff8d4691b8e2c7",
+                33578009.toBigInteger() to "0xd6b53a04d6dc0e8eb2a9fb63b474211ae6660f31fc1cb4775733552948d0c3d6",
+                33577578.toBigInteger() to "0xfd4600ba9f7d41f5f51e156c03b8b82fb72de7e9171d049bcb54e12f1ccb1c54"
+            ) { (block, hash) ->
+                val trace = rateLimiter.executeFunction { txTrace(block, hash) }
+                trace.from shouldNotBe ""
+                trace.to shouldNotBe ""
+                trace.calls shouldHaveAtLeastSize 5
+            }
+        }
     }
 
 })
